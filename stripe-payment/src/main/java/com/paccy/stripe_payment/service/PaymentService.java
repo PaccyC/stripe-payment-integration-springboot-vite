80b1c32de981f53c896df02cb8e2ae6f90d229c0
PaccyC <abayisengaaimepacifique@gmail.com>
2025-05-24 17:14:23 +0200

    added hosted checkout

--- stripe-payment/src/main/java/com/paccy/stripe_payment/service/PaymentService.java
+++ stripe-payment/src/main/java/com/paccy/stripe_payment/service/PaymentService.java
@@ -3,10 +3,13 @@ package com.paccy.stripe_payment.service;
 import com.paccy.stripe_payment.dao.ProductDAO;
 import com.paccy.stripe_payment.dto.RequestDto;
 import com.paccy.stripe_payment.utils.CustomerUtil;
+import com.stripe.Stripe;
 import com.stripe.exception.StripeException;
 import com.stripe.model.Customer;
+import com.stripe.model.PaymentIntent;
 import com.stripe.model.Product;
 import com.stripe.model.checkout.Session;
+import com.stripe.param.PaymentIntentCreateParams;
 import com.stripe.param.checkout.SessionCreateParams;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
@@ -18,6 +21,8 @@ public class PaymentService {
 
     @Value("${stripe.secretKey}")
     String stripeApiKey;
+
+
 
     @Value("${client.baseUrl}")
     String clientBaseUrl;
@@ -60,5 +65,38 @@ public class PaymentService {
         Session session= Session.create(paramsBuilder.build());
 
         return session.getUrl();
+    }
+
+    public String integratedCheckout(RequestDto requestDto) throws StripeException {
+
+
+        Customer customer= CustomerUtil.findOrCreateCustomer(requestDto.getCustomerEmail(), requestDto.getCustomerName());
+
+        // Create a PaymentIntent and send it's client secret to the client
+        PaymentIntentCreateParams  params= PaymentIntentCreateParams
+                .builder()
+                .setAmount(Long.parseLong(this.calculateOrderAmount(requestDto.getItems())))
+                .setCurrency("usd")
+                .setCustomer(customer.getId())
+                .setAutomaticPaymentMethods(
+                        PaymentIntentCreateParams.AutomaticPaymentMethods
+                                .builder()
+                                .setEnabled(true)
+                                .build()
+                )
+                .build();
+        PaymentIntent paymentIntent= PaymentIntent.create(params);
+        return  paymentIntent.getClientSecret();
+    }
+
+    String calculateOrderAmount(Product[] items){
+
+        long total=0L;
+        for (Product item:items){
+            // Look up the application database to find the prices for the products in the given list
+            total +=ProductDAO.getProduct(item.getId()).getDefaultPriceObject().getUnitAmountDecimal().floatValue();
+
+        }
+        return String.valueOf(total);
     }
 }
