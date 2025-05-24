c5957836cdf7882f4343fba8777098812b7f7788
PaccyC <abayisengaaimepacifique@gmail.com>
2025-05-24 17:14:23 +0200

    added hosted checkout

--- stripe-payment/src/main/java/com/paccy/stripe_payment/controller/PaymentController.java
+++ stripe-payment/src/main/java/com/paccy/stripe_payment/controller/PaymentController.java
@@ -5,6 +5,7 @@ import com.paccy.stripe_payment.dto.RequestDto;
 import com.paccy.stripe_payment.service.PaymentService;
 import com.stripe.exception.StripeException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,9 +17,19 @@ public class PaymentController {
     private final PaymentService paymentService;
 
     @PostMapping("/checkout/hosted")
+    @CrossOrigin(origins = "http://localhost:5173")
     public String checkoutSession(@RequestBody RequestDto requestDto) throws StripeException {
 
         return paymentService.hostedCheckout(requestDto);
+
+
+    }
+
+    @CrossOrigin(origins = "http://localhost:5173")
+    @PostMapping("/checkout/integrated")
+    public String integratedCheckout(@RequestBody RequestDto requestDto) throws StripeException {
+
+        return paymentService.integratedCheckout(requestDto);
 
 
     }
